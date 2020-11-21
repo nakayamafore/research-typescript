43664b395ed0c634a64e8725982bba8bbd2cc7b3
中山　宏一 <nakayama@forefrontier.co.jp>
2020-11-21 11:05:52 +0900

    add gender birthday input

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { TextField } from "@material-ui/core";
+import {
+    TextField,
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    FormControlLabel,
+    Radio
+} from "@material-ui/core";
 import useStyles from "./styles";
 import { RootState } from "../domain/entity/rootState";
 import { Profile } from "../domain/entity/profile";
+import { Gender } from "../domain/entity/gender";
 import profileActions from "../store/profile/actions";
 import { PROFILE } from "../domain/services/profile";
 
@@ -21,6 +29,16 @@ const Basic = () => {
                 value={profile.name} onChange={e => handleChange({ name: e.target.value })} />
             <TextField fullWidth multiline className={classes.formField} rows={5} label={PROFILE.DESCRIPTION}
                 value={profile.description} onChange={e => handleChange({ description: e.target.value })} />
+            <FormControl className={classes.formField}>
+                <FormLabel>{PROFILE.GENDER}</FormLabel>
+                <RadioGroup value={profile.gender} onChange={e => handleChange({ gender: e.target.value as Gender })}>
+                    <FormControlLabel value="male" label="男性" control={<Radio color="primary" />} />
+                    <FormControlLabel value="female" label="女性" control={<Radio color="primary" />} />
+                </RadioGroup>
+            </FormControl>
+            <TextField fullWidth className={classes.formField} label={PROFILE.BIRTHDAY} type="date" InputLabelProps={{ shrink: true }}
+                value={profile.birthday} onChange={e => handleChange({ birthday: e.target.value })}
+            />
         </>
     )
 }

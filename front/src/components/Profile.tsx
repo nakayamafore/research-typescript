b145bf87aabdf0a1c2e3a127de6c07d91f9f5028
中山　宏一 <nakayama@forefrontier.co.jp>
2020-11-21 15:17:31 +0900

    add address input

--- front/src/components/Profile.tsx
+++ front/src/components/Profile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Typography } from "@material-ui/core";
 
 import Basic from "./Basic";
+import Address from "./Address";
 import useStyles from "./styles";
 
 const Profile = () => {
@@ -12,6 +13,10 @@ const Profile = () => {
                 基本情報
             </Typography>
             <Basic />
+            <Typography variant="h4" component="h2" className={classes.title} color="primary">
+                住所
+            </Typography>
+            <Address />
         </Container>
     )
 }

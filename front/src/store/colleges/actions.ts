2c53a26573ceb3b96294d5249c70bf89fd0901c0
中山　宏一 <nakayama@forefrontier.co.jp>
2020-11-25 07:32:43 +0900

    add college select

--- front/src/store/colleges/actions.ts
+++ front/src/store/colleges/actions.ts
@@ -1,8 +1,10 @@
 import actionCreatorFactory from "typescript-fsa";
+import { CollegeResult } from "../../domain/entity/college";
 
 const actionCreator = actionCreatorFactory();
 
 const collegesActions = {
-    setSearchWord: actionCreator<string>("SET_SEARCH_WORD")
+    setSearchWord: actionCreator<string>("SET_SEARCH_WORD"),
+    searchCollege: actionCreator.async<{}, CollegeResult[], {}>("SEARCH_COLLEGE")
 }
 export default collegesActions;
